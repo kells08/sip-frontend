c7b4745baf93136753d5c29cfd582d5a7e810124
github username <github email address>
2018-09-14 15:03:51 -0500

    some login code

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function showAllDrinks(drinks) {
 
 function showOneDrink(drink) {
   drinksDiv.innerHTML += `
-      <article id=${drink.id}> 
+      <article id=${drink.id}>
         <div class="image round">
           <img src="${drink.image}" width="200" height="auto" />
         </div>
@@ -36,17 +36,46 @@ function showDrinks() {
   window.scrollTo(300, 500);
 }
 
+////////////////////////////////////////////////
+
+
+
+const loginBtn = document.querySelector('#login-button')
+const loginForm = document.querySelector('.container')
+let adduserLogin = false
+const submitButton = document.querySelector('.login-form')
+
+
+loginBtn.addEventListener('click', () => {
+  // hide & seek with the form
+  addLoginForm = !addLoginForm
+  if (addLoginForm) {
+    loginForm.style.display = 'block'
+
+  } else {
+    loginForm.style.display = 'none'
+  }
+})
+
+submitButton.addEventListener('submit', handleLoginForm)
+
+function handleLoginForm (e){
+  e.preventDefault(e)
+  updateLoginForm(e)
+  postLoginForm(e)
+}
+
+
+function updatetLoginForm(e){
+  let nameInput = document.querySelector('#user-name')
+  let studentTrue = document.querySelector('#flatiron-student')
+  let studentFalse = document.querySelector('#not-a-flatiron-student')
 
 
 
 
+}
 
+function postLoginForm(e){
 
-
-      // json.forEach(drink => {
-        // const markup = `
-        // <li>
-        //   <h3>${drink.name}
-        //     <button>edit</button>
-        //   </h3>
-        // </li>`;
+}
